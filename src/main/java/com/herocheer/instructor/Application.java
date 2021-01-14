ca70fe84086a151315770f62b5f3de6f97517830
gaorh <781975395@qq.com>
2021-01-14 11:40:13 +0800

    style(系统管理):补充启动类

--- src/main/java/com/herocheer/instructor/Application.java
+++ src/main/java/com/herocheer/instructor/Application.java
@@ -3,16 +3,27 @@ package com.herocheer.instructor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author chenwf
  * @date 2020/12/17
  */
-@SpringBootApplication(scanBasePackages = {"com.herocheer"})
+@SpringBootApplication(scanBasePackages = {"com.herocheer"},exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @MapperScan(basePackages = {"com.herocheer.instructor.dao"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 将加密工具类加入IOC容器中,便于加密
+     */
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
