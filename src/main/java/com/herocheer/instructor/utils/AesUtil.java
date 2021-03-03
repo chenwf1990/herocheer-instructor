27530b2cd28717c92437a26cff1c933a80ff4008
chenwf <linfan0529@gmail.com>
2021-03-03 14:08:29 +0800

    修复bug

--- src/main/java/com/herocheer/instructor/utils/AesUtil.java
+++ src/main/java/com/herocheer/instructor/utils/AesUtil.java
@@ -1,15 +1,12 @@
 package com.herocheer.instructor.utils;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.util.Base64Utils;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.Security;
 
 /**
  * @author chenwf
@@ -19,70 +16,58 @@ import java.security.SecureRandom;
  */
 @Slf4j
 public class AesUtil {
-    private static final String KEY_ALGORITHM = "AES";
-    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";//默认的加密算法
+    private static final String ENCODE = "UTF-8";
+    private static final String FORMAT = "AES";
+    private static final String CIPHER_ALGORITHM = "AES/ECB/PKCS7Padding";
 
     public static final String secretKey = "e6a24eb4a7a741a795ecc0a3e35caef2";
 
-    /**
-     * AES 加密操作
-     *
-     * @param content  待加密内容
-     * @return 返回Base64转码后的加密数据
-     */
-    public static String encrypt(String content) {
-        try {
-            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器
-            byte[] byteContent = content.getBytes("utf-8");
-            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(secretKey));// 初始化为加密模式的密码器
-            byte[] result = cipher.doFinal(byteContent);// 加密
-            return Base64Utils.encodeToString(result);
-        } catch (Exception ex) {
-            log.error("加密失败");
-        }
-
-        return null;
+    static {
+        Security.addProvider(new BouncyCastleProvider());
     }
 
+
     /**
-     * AES 解密操作
-     *
+     * 加密
      * @param content
      * @return
      */
-    public static String decrypt(String content) {
-        try {
-            //实例化
-            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            //使用密钥初始化，设置为解密模式
-            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(secretKey));
-            //执行操作
-            byte[] result = cipher.doFinal(Base64Utils.decodeFromString(content));
-            return new String(result, "utf-8");
-        } catch (Exception ex) {
-            log.error("解密失败");        }
-
+    public static String encrypt(String content){
+        try{
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            byte[] raw = secretKey.getBytes();
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, FORMAT);
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+            byte[] encrypted = cipher.doFinal(content.getBytes(ENCODE));
+            return Base64Utils.encodeToString(encrypted);
+        } catch (Exception e) {
+            log.error("解密异常 ",e);
+            e.printStackTrace();
+        }
         return null;
     }
 
-    /**
-     * 生成加密秘钥
-     *
-     * @return
-     */
-    private static SecretKeySpec getSecretKey(final String password) {
-        //返回生成指定算法密钥生成器的 KeyGenerator 对象
-        KeyGenerator kg = null;
+    public static String decrypt(String str){
         try {
-            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
-            //AES 要求密钥长度为 128
-            kg.init(128, new SecureRandom(password.getBytes()));
-            //生成一个密钥
-            SecretKey secretKey = kg.generateKey();
-            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥
-        } catch (NoSuchAlgorithmException ex) {
-            log.error("加密失败");
+
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(ENCODE), FORMAT);
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+            byte[] result = cipher.doFinal(Base64Utils.decodeFromString(str));
+            return new String(result, ENCODE);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        String str = "350521199007031038";
+        String a = encrypt(str);
+        System.out.println(a);
+        System.out.println(decrypt(a));
     }
 }
