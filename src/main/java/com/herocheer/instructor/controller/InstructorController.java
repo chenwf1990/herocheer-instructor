38d482d1ad2046172b5940ff9a17496bd06cf31a
gaorh <781975395@qq.com>
2021-04-12 11:37:00 +0800

    refactor(系统管理)：采集系统消息

--- src/main/java/com/herocheer/instructor/controller/InstructorController.java
+++ src/main/java/com/herocheer/instructor/controller/InstructorController.java
@@ -9,11 +9,19 @@ import com.herocheer.web.base.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author chenwf
@@ -58,4 +66,15 @@ public class InstructorController extends BaseController{
         return ResponseResult.ok();
     }
 
+    /**
+     * 获取所有指导员
+     *
+     * @param request 请求
+     * @return {@link ResponseResult<Instructor>}
+     */
+    @GetMapping("/instructores")
+    @ApiOperation("所有指导员")
+    public ResponseResult<List<Instructor>> fetchInstructorAll(HttpServletRequest request){
+        return ResponseResult.ok(instructorService.findByLimit(new HashMap<String,Object>()));
+    }
 }
