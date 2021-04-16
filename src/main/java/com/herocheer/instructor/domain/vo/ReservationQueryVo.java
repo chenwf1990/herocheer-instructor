8a90de6535800b19d1020fc51b4d51142f94dc88
chenwf1990 <linfan0529@gmail.com>
2021-04-16 11:04:34 +0800

    预约信息查询新增 用户名和身份证查询

--- src/main/java/com/herocheer/instructor/domain/vo/ReservationQueryVo.java
+++ src/main/java/com/herocheer/instructor/domain/vo/ReservationQueryVo.java
@@ -14,6 +14,12 @@ import java.io.Serializable;
 @Data
 public class ReservationQueryVo implements Serializable {
 
+    @ApiModelProperty("姓名")
+    private String name;
+
+    @ApiModelProperty("身份证号")
+    private String identityNumber;
+
     @ApiModelProperty("查询类型(0.全部预约记录1.招募或课程的预约记录3.个人的预约记录)")
     private Integer queryType;
 
